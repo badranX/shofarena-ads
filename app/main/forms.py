86d491e0d0b25aa5989a19808d45d759efc9fcfb
badran <yahya.badran@gmail.com>
2019-03-07 15:33:18 -0800

    -Fixed hidden text in SelectText -Added mahmoud's work to the project (city names) -Did some changes to one of the DyanmicCities.js

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,9 +9,9 @@ from wtforms.validators import DataRequired,InputRequired,Email,Length, Regexp
 class SignUp(FlaskForm):
     
     #Name
-    first_name=StringField('What is your name?',validators=[DataRequired()])
-    father_name=StringField('father name?',validators=[DataRequired()])
-    family=StringField('family name?',validators=[DataRequired()])
+    first_name=StringField('الإسم',validators=[DataRequired()])
+    father_name=StringField('إسم الأب',validators=[DataRequired()])
+    family=StringField('إسم العائلة',validators=[DataRequired()])
     
     #ID Number
     id=StringField('id',
@@ -26,8 +26,7 @@ class SignUp(FlaskForm):
         render_kw= {
             "pattern":"^\+?\d{10,14}$",
             "type":"tel"},
-        validators=[InputRequired(), 
-        Regexp("^\+?\d{10,14}$",message='number is wrong')])
+        validators=[InputRequired(), Regexp("^\+?\d{10,14}$",message='number is wrong')])
 
     #Email
     email=StringField('Email',
@@ -37,49 +36,137 @@ class SignUp(FlaskForm):
         Email(message=u'That\'s not a valid email address.')])
 
     #home_adress
-    home_city = SelectField(u'Programming Language', 
-        choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')],
+    home_city = SelectField(u'المدينة', 
+        choices=[('', 'المحافظة'),
+            ('اريحا', 'اريحا'), 
+            ('الخليل', 'الخليل'),
+            ('القدس', 'القدس'),
+            ('بيت لحم', 'بيت لحم'),
+            ('جنين', 'جنين'),
+            ('رام الله والبيرة ', 'رام الله والبيرة'),
+            ('قلقيلية', 'قلقيلية'),
+            ('نابلس', 'نابلس'),
+            ('قطاع غزة', 'قطاع غزة')
+         ],
         validators = [DataRequired()])
-    home_town = SelectField(u'Programming Language', 
-        choices=[('','Village'),('del','del')],
+    home_town = SelectField(u'البلدة', 
+        choices=[('','البلدة')],
         validators = [DataRequired()])
 
     #work_adress
-    work_city = SelectField(u'Programming Language', 
-        choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')],
+    work_city = SelectField(u'البلدة', 
+        choices=[('', 'المحافظة'),
+            ('اريحا', 'اريحا'), 
+            ('الخليل', 'الخليل'),
+            ('القدس', 'القدس'),
+            ('بيت لحم', 'بيت لحم'),
+            ('جنين', 'جنين'),
+            ('رام الله والبيرة ', 'رام الله والبيرة'),
+            ('قلقيلية', 'قلقيلية'),
+            ('نابلس', 'نابلس'),
+            ('قطاع غزة', 'قطاع غزة')
+         ],
         validators = [DataRequired()])
-    work_town = SelectField(u'Programming Language',
-        choices=[('','town'), ('del','del')],
+    work_town = SelectField(u'البلدة',
+        choices=[('','البلدة')],
         validators = [DataRequired()])
     #car_taxi_private
-    car_type = SelectField(u'Programming Language', 
-        choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')],
+    car_type = SelectField(u'نوع السيارة', 
+        choices=[
+            ("",'نوع السيارة'),
+            ('private', 'خاصة'),
+            ('taxi', 'تكسي'),
+            ('van', 'شاحنة'),
+         ],
         validators = [DataRequired()])
 
     #car_year
-    car_year = SelectField(u'Programming Language', 
-        choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')],
+    car_year = SelectField(u'السنة', 
+        choices=[('', 'السنة'),
+            ('2016', '2016'),
+            ('2015', '2015'),
+            ('2014', '2014'),
+            ('2013', '2013'),
+            ('2012', '2012'),
+            ('2011', '2011'),
+            ('2010', '2010'),
+            ('2009', '2009'),
+            ('2008', '2008'),
+            ('2007', '2007'),
+            ('2006', '2006'),
+            ('2005', '2005'),
+            ('2004', '2004')
+          ],
         validators = [DataRequired()])
 
     #car_company #car_model
-    car_company = SelectField(u'Programming Language', 
-        choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')],
+    car_company = SelectField(u'نوع السيارة', 
+        choices=[('', 'اختار'), 
+            ('dodge', 'Dodge'),
+            ('اوبل', 'اوبل'),
+            ('اودي', 'اودي'),
+            ('ايسوزو', 'ايسوزو'),
+            ('باص', 'باص'),
+            ('بي ام دبليو BMW', 'بي ام دبليو BMW'),
+            ('بيجو', 'بيجو'),
+            ('تويوتا', 'تويوتا'),
+            ('جي ام سي', 'جي ام سي'),
+            ('جيب jeep', 'جيب Jeep'),
+            ('داتشيا', 'داتشيا'),
+            ('داف', 'داف'),
+            ('دايو', 'دايو'),
+            ('دراجة نارية', 'دراجة نارية'),
+            ('ديهاتسو', 'ديهاتسو'),
+            ('روفر', 'روفر'),
+            ('رينو', 'رينو'),
+            ('سانغ يونغ', 'سانغ يونغ'),
+            ('ستروين', 'ستروين'),
+            ('سكودا', 'سكودا'),
+            ('سوبارو', 'سوبارو'),
+            ('سوزوكي', 'سوزوكي'),
+            ('سيت', 'سيت'),
+            ('شاحنة', 'شاحنة'),
+            ('شفرليت', 'شفرليت'),
+            ('فورد', 'فورد'),
+            ('فولفو', 'فولفو'),
+            ('فولكسفاجن', 'فولكسفاجن'),
+            ('كاديلاك', 'كاديلاك'),
+            ('كرايسلر', 'كرايسلر'),
+            ('kia', 'كيا Kia'),
+            ('لاندروفر', 'لاندروفر'),
+            ('مازدا', 'مازدا'),
+            ('مرسيدس', 'مرسيدس'),
+            ('ميتسوبيشي', 'ميتسوبيشي'),
+            ('نيسان', 'نيسان'),
+            ('هوندا', 'هوندا'),
+            ('هونداي', 'هونداي')],
         validators = [DataRequired()])
 
     
-    car_model = SelectField(u'Programming Language',
-        choices=[('','carModeel'), ('del','del')],
+    car_model = SelectField(u'موديل السيارة',
+        choices=[('','الموديل')],
         validators = [DataRequired()])
     #car_color
-    car_color = SelectField(u'Programming Language', 
-        choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')],
+    car_color = SelectField(u'لون السيارة', 
+        choices=[('','اللون'),
+            ('أسود', 'أسود'),
+            ('فضي', 'فضي'),
+            ('أبيض', 'أبيض'),
+            ('أزرق', 'أزرق'),
+            ('أحمر', 'أحمر'),
+            ('أصفر', 'أصفر'),
+            ('أخرى', 'أخرى')],
         validators = [DataRequired()])
 
     #car_condition
-    car_condition =  SelectField(u'Programming Language', 
-        choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')],
+    car_condition =  SelectField(u'حالة السيارة', 
+        choices=[('', 'الحالة'),
+            ('ممتازة', 'ممتازة'),
+            ('جيدة', 'جيدة'),
+            ('سيئة', 'سيئة')
+         ],
         validators = [DataRequired()])
     #SUBMIT
-    submit = SubmitField('Submit')
+    submit = SubmitField('تسجيل')
 
     
--- app/main/views.py
+++ app/main/views.py
@@ -8,32 +8,26 @@ from .forms import SignUp
 
 @main_blueprint.route('/sign', methods=['GET','POST'])
 def signup():
-    flash("hilo")
     
     form = SignUp()
     
-    if request.method == 'POST':
-        print(request.form.to_dict())
-    usr = mongo.db.driver.find_one_or_404({})
+    #if request.method == 'POST':
+        #print(request.form.to_dict())
+    #usr = mongo.db.driver.find_one_or_404({})
 
     if form.validate_on_submit():
-        print("validated YAHYA")
         mongo.db.driver.insert_one(request.form.to_dict())
         return redirect(url_for('static', filename='registerdone.html'))
 
     flash_errors(form)
-    return render_template('form.html',form=form,userr=usr)
+    return render_template('base.html',form=form)
 
 
 def flash_errors(form):
     
     for field, errors in form.errors.items():
         for error in errors:
-            
-            flash(u"Error in the %s field - %s" % (
-                getattr(form, field).label.text,
-                error
-            ))
+            flash(u"Error in the %s field - %s" % (getattr(form, field).label.text), error)
 
 
 
